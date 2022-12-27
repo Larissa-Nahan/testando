5b65376fc47552cd8c494bf9be390f003f3f0e9a
Larissa-Nahan <larissanahan@gmail.com>
2022-12-27 09:06:01 -0300

    fieldset titulo criterio selecao

--- static/admin/js/perguntas.js
+++ static/admin/js/perguntas.js
@@ -7,22 +7,22 @@ window.addEventListener('load', function () {
         var campo_criterio = document.querySelector('.field-criterio');
         var field = document.createElement('fieldset');
         field.classList.add('module', 'aligned')
-        field.innerHTML = '<h2>' + campo_criterio.innerHTML + '</h2>';
+        field.innerHTML = campo_criterio.innerHTML;
         inicio.after(field)
         
         campo_criterio.remove()
         document.querySelector('label[for="id_criterio"]').remove();
         
-        var dropdown_criterio = document.querySelector('#id_criterio');
-        // console.log(dropdown_criterio.value.options)
+        $('div[data-model-ref="criterio"]').unwrap();
+        $('div[data-model-ref="criterio"]').replaceWith( "<h2>" + $('div[data-model-ref="criterio"]').html() + "</h2>" );
         
+        var dropdown_criterio = document.querySelector('#id_criterio');
         function onChange() {
         for(var j = 0; j < document.querySelector('#id_avaliacao_chefes').childElementCount; j++){
             // for(var i = 1; i < dropdown_criterio.length; i++){
                 var pergunta = document.querySelector('#id_avaliacao_chefes_'+[j]).nextSibling.nodeValue;
                 var criterio_pergunta = pergunta.split(' ').splice(-2).join(" ");
                 
-                    var value = dropdown_criterio.value;
                     var text = dropdown_criterio.options[dropdown_criterio.selectedIndex].text;
                     
                     if(criterio_pergunta == text){
