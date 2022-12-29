8b106eb4dad1f675132f0c446365aff88b192d9e
Larissa-Nahan <larissanahan@gmail.com>
2022-12-29 08:55:52 -0300

    add anotacoes

--- static/admin/js/perguntas.js
+++ static/admin/js/perguntas.js
@@ -2,19 +2,19 @@ django.jQuery = jQuery;
 
 window.addEventListener('load', function () {
     django.jQuery(document).ready(function () {
-        
-        
-        
+        // pega o campo criterio (inicialmente junto com os dados do usuario)
         var inicio = document.querySelector('#content-main').querySelector('#avaliarcolaborador_form').querySelector('.module');
         var campo_criterio = document.querySelector('.field-criterio');
         var fieldsets = document.querySelectorAll('fieldset');
         fieldsets[1].insertAdjacentHTML('afterbegin', campo_criterio.innerHTML);
         inicio.after(fieldsets[1])
         
+        // remove o campo de criterio da parte do usuario e deleta o conteudo anterior
         campo_criterio.remove()
         document.querySelector('label[for="id_criterio"]').remove();
         $('h2')[1].remove();
-        
+
+        // insere o campo no h2 titulo
         $('div[data-model-ref="criterio"]').unwrap();
         $('div[data-model-ref="criterio"]').replaceWith( "<h2>" + $('div[data-model-ref="criterio"]').html() + "</h2>" );
         
@@ -34,12 +34,12 @@ window.addEventListener('load', function () {
         // coloca as imgs dentro dos btn e os insere ao lado do criterio no h2
         btn_seta_esquerda.appendChild(seta_esquerda)
         btn_seta_direita.appendChild(seta_direita)
-
         $("#id_criterio")[0].after(btn_seta_esquerda);
         btn_seta_esquerda.after(btn_seta_direita);
 
         var dropdown_criterio = document.querySelector('#id_criterio');
 
+        // funcao para exibir ou esconder as perguntas
         var showElement = function(el, display) {
             el.style.display = display ? '' : 'none';
         };
@@ -50,17 +50,17 @@ window.addEventListener('load', function () {
         var input = [];
         for(var j = 0; j < document.querySelector('#id_avaliacao_chefes').childElementCount; j++){
             topicos[j] = $('label[for=id_avaliacao_chefes_'+[j]+']')[0]
-            criterios[j] = topicos[j].innerText.split(' ').splice(-2).join(" ");
-            perguntas[j] = topicos[j].innerText.split(" - ")
-            topicos[j].value = criterios[j]
+            criterios[j] = topicos[j].innerText.split(' ').splice(-2).join(" ");  // pega e armazena somente o critério
+            perguntas[j] = topicos[j].innerText.split(" - ")  // divide o que vem escrito [escala - pergunta - criterio]
             input[j] = $('#id_avaliacao_chefes_'+[j])[0]
-            topicos[j].innerHTML = perguntas[j][1]
-            topicos[j].insertBefore(input[j], topicos[j].firstChild);
+            topicos[j].innerHTML = perguntas[j][1]  // suubstitui o conteudo anterior por somente as perguntas
+            topicos[j].insertBefore(input[j], topicos[j].firstChild);  // insere o input (checkbox) no inicio das perguntas
             showElement($('label[for=id_avaliacao_chefes_'+[j]+']')[0], false);
         }
 
+        // funcao para mudar as perguntas exibidas a partir da selecao do dropdown
         function onChange() {
-            var text = dropdown_criterio.options[dropdown_criterio.selectedIndex].text;
+            var text = dropdown_criterio.options[dropdown_criterio.selectedIndex].text;  // pegas as opcoes do dropdown
             for(var j = 0; j < document.querySelector('#id_avaliacao_chefes').childElementCount; j++){
                 if(criterios[j] === text){
                     showElement(topicos[j], true);
