b80741cf9da0913cde201008ecd236f15387d967
Larissa-Nahan <larissanahan@gmail.com>
2022-12-29 08:46:40 -0300

    btns avancar e voltar - inicio

--- static/admin/js/perguntas.js
+++ static/admin/js/perguntas.js
@@ -3,6 +3,8 @@ django.jQuery = jQuery;
 window.addEventListener('load', function () {
     django.jQuery(document).ready(function () {
         
+        
+        
         var inicio = document.querySelector('#content-main').querySelector('#avaliarcolaborador_form').querySelector('.module');
         var campo_criterio = document.querySelector('.field-criterio');
         var fieldsets = document.querySelectorAll('fieldset');
@@ -16,6 +18,26 @@ window.addEventListener('load', function () {
         $('div[data-model-ref="criterio"]').unwrap();
         $('div[data-model-ref="criterio"]').replaceWith( "<h2>" + $('div[data-model-ref="criterio"]').html() + "</h2>" );
         
+        // cria os btn e add ids
+        const btn_seta_esquerda = document.createElement("button");
+        const btn_seta_direita = document.createElement("button");
+        btn_seta_esquerda.setAttribute("id", "btn_seta_esquerda")
+        btn_seta_direita.setAttribute("id", "btn_seta_direita")
+        // cria as imagens e add a scr + tamanho
+        const seta_esquerda = document.createElement("img");
+        const seta_direita = document.createElement("img");
+        seta_esquerda.src = "/static/admin/imgs/arrow_left.png" 
+        seta_direita.src = "/static/admin/imgs/arrow_right.png"
+        seta_esquerda.height = 25
+        seta_direita.height = 25
+
+        // coloca as imgs dentro dos btn e os insere ao lado do criterio no h2
+        btn_seta_esquerda.appendChild(seta_esquerda)
+        btn_seta_direita.appendChild(seta_direita)
+
+        $("#id_criterio")[0].after(btn_seta_esquerda);
+        btn_seta_esquerda.after(btn_seta_direita);
+
         var dropdown_criterio = document.querySelector('#id_criterio');
 
         var showElement = function(el, display) {
@@ -34,7 +56,6 @@ window.addEventListener('load', function () {
             input[j] = $('#id_avaliacao_chefes_'+[j])[0]
             topicos[j].innerHTML = perguntas[j][1]
             topicos[j].insertBefore(input[j], topicos[j].firstChild);
-            console.log(topicos[j].innerHTML)
             showElement($('label[for=id_avaliacao_chefes_'+[j]+']')[0], false);
         }
 
